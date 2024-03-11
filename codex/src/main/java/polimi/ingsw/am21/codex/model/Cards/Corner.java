23a13944a442084e6b0c57b87688263a552c704f
[Sara Berardinelli] <sara.berardinelli@mail.polimi.it>
2024-03-11 16:25:04 +0100

    feat[model]: implement class corner

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/Corner.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/Corner.java
@@ -1,20 +1,32 @@
 package polimi.ingsw.am21.codex.model.Cards;
 
+import java.util.Optional;
+
 public class Corner<T> {
-    T content;
-    PlayedCard linkedCard;
-    
-    void setLinkedCard(PlayedCard card){
-        this.linkedCard = card;
+    Optional<T> contentType;
+    CornerEnum cornerNumber;
+    Optional<T> content;
+
+    Optional<PlayedCard> linkedCard;
+
+    public Corner(T content){
+        this.content = Optional.of(content);
     }
 
-    private PlayedCard getLinkedCard() {
+    boolean isLinked() {
+        return linkedCard.isPresent();
+    }
+
+    public Optional<T> getActualContent() {
+        return contentType;
+    }
+    void setLinkedCard(PlayedCard card){
+        this.linkedCard = Optional.of(card);
+    }
+
+    private Optional<PlayedCard> getLinkedCard() {
         return linkedCard;
     }
 
-
-    public Corner(T content){
-        this.content = content;
-    }
 
 }
