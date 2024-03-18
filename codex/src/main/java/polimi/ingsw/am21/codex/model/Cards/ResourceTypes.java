2497a2a9b0614faee136fb363e5327547e4524e3
frephs <francesco1.genovese@mail.polimi.it>
2024-03-18 11:20:41 +0100

    feat(model): implement generic has() method for resource/object recognition of enums passed as attribute.

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/ResourceTypes.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/ResourceTypes.java
@@ -1,8 +1,16 @@
 package polimi.ingsw.am21.codex.model.Cards;
 
+import java.util.Arrays;
+
 public enum ResourceTypes {
     PLANT_KINGDOM,
     ANIMAL_KINGDOM,
     FUNGI_KINGDOM,
-    INSECT_KINGDOM
+    INSECT_KINGDOM;
+
+    public static <T> boolean has(T enumObject){
+        return Arrays.stream(ResourceTypes.values()).anyMatch(
+                value -> value == enumObject
+        );
+    }
 }
