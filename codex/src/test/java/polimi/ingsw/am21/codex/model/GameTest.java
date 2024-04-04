bdafb4e1daaaf3418b2229dc58a59b4348c5f569
hamza.haddou <hamza.haddou@mail.polimi.it>
2024-04-04 16:31:03 +0200

    feat: started adding tests

--- codex/src/test/java/polimi/ingsw/am21/codex/model/GameTest.java
+++ codex/src/test/java/polimi/ingsw/am21/codex/model/GameTest.java
@@ -2,15 +2,28 @@ package polimi.ingsw.am21.codex.model;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    Game game;
+
+    @BeforeEach
+    void init(){
+        game = new Game(2, new JSONArray());
+    }
+
     @Test
-    void  lobby() {
+    void testGetPlayers() {
+        assertEquals(2, game.getPlayers().size());
+    }
 
-
+    @Test
+    void testGetPlayer() {
+        assertEquals("Player1", game.getPlayer(0).getName());
     }
 
 }
