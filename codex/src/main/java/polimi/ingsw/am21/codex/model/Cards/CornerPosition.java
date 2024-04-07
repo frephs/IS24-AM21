be1b2a6fc289d9a248b13a852682391773c4113d
frephs <francesco1.genovese@mail.polimi.it>
2024-04-07 10:39:23 +0200

    fix(model, corners): change naming of enum from up to top and down to bottom

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/CornerPosition.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/CornerPosition.java
@@ -1,8 +1,15 @@
 package polimi.ingsw.am21.codex.model.Cards;
 
 public enum CornerPosition {
-    UP_LEFT,
-    DOWN_LEFT,
-    UP_RIGHT,
-    DOWN_RIGHT
+    TOP_LEFT(0),
+    BOTTOM_LEFT(1),
+    TOP_RIGHT(2),
+    BOTTOM_RIGHT(3);
+
+    private final int index;
+
+    CornerPosition(int index) {
+        this.index = index;
+    }
+
 }
