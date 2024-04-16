bbff06c2bdd7bb909da5ba74fdac7abc536140b0
[Sara Berardinelli] <sara.berardinelli@mail.polimi.it>
2024-04-16 14:10:56 +0200

    FEAT(): test implementation for the geometricObjective

---
+++ codex/src/test/java/polimi/ingsw/am21/codex/model/Cards/GeometricObjectiveTest.java
@@ -0,0 +1,30 @@
+package polimi.ingsw.am21.codex.model.Cards;
+
+import org.junit.jupiter.api.Test;
+import polimi.ingsw.am21.codex.model.PlayerBoard;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static polimi.ingsw.am21.codex.model.Cards.CornerPosition.BOTTOM_LEFT;
+
+class GeometricObjectiveTest {
+
+  @Test
+  void getEvaluator() {
+    Map<AdjacentPosition, ResourceType> geometry = new HashMap<>();
+    geometry.put(BOTTOM_LEFT, ResourceType.FUNGI_KINGDOM);
+    GeometricObjective geometricObjective = new GeometricObjective(geometry);
+
+    PlayerBoard pb = new PlayerBoard(
+      new ArrayList<>(),
+      new PlayableCard(123, null, null),
+      new ObjectiveCard(123, 123, null)
+    );
+
+    //evaluator should return 0
+    assertEquals(geometricObjective.getEvaluator().apply(pb, 123), 0);
+  }
+}
