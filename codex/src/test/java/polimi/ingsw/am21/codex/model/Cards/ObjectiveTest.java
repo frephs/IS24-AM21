390140281a1a90a2644d8b050fbdf30af11f874e
[Sara Berardinelli] <sara.berardinelli@mail.polimi.it>
2024-04-16 14:30:42 +0200

    feat(): test

---
+++ codex/src/test/java/polimi/ingsw/am21/codex/model/Cards/ObjectiveTest.java
@@ -0,0 +1,12 @@
+package polimi.ingsw.am21.codex.model.Cards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ObjectiveTest {
+
+  @Test
+  void getEvaluator() {
+  }
+}
