5f609a778797afb5547876595d97119e583e7fdc
[Sara Berardinelli] <sara.berardinelli@mail.polimi.it>
2024-04-16 14:30:54 +0200

    feat(): test

---
+++ codex/src/test/java/polimi/ingsw/am21/codex/model/Cards/PlayableSideTest.java
@@ -0,0 +1,24 @@
+package polimi.ingsw.am21.codex.model.Cards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayableSideTest {
+
+  @Test
+  void getCorners() {
+  }
+
+  @Test
+  void setCorners() {
+  }
+
+  @Test
+  void getEvaluator() {
+  }
+
+  @Test
+  void getPlaceabilityChecker() {
+  }
+}
