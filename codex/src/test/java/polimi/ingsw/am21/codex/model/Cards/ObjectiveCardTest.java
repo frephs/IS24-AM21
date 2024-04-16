2b1be7da02a618d057061ca300e8f003c1202c4d
[Sara Berardinelli] <sara.berardinelli@mail.polimi.it>
2024-04-16 14:42:44 +0200

    FEAT(): test

---
+++ codex/src/test/java/polimi/ingsw/am21/codex/model/Cards/ObjectiveCardTest.java
@@ -0,0 +1,17 @@
+package polimi.ingsw.am21.codex.model.Cards;
+
+import org.junit.jupiter.api.Test;
+import polimi.ingsw.am21.codex.model.PlayerBoard;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ObjectiveCardTest {
+
+  @Test
+  void getEvaluator() {
+
+
+  }
+}
