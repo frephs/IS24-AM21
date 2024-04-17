c858c461ace228cdafb8c845e14db9def39d4361
hamza.haddou <hamza.haddou@mail.polimi.it>
2024-04-17 19:35:14 +0200

    fix: typo

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/MissingParametersException.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/MissingParametersException.java
@@ -2,6 +2,6 @@ package polimi.ingsw.am21.codex.model.Cards;
 
 public class MissingParametersException extends IllegalStateException {
     MissingParametersException(String missingAttribute) {
-        super("Attribute " + missingAttribute + "is supposed to be set first");
+        super("Attribute " + missingAttribute + " is supposed to be set first");
     }
 }
