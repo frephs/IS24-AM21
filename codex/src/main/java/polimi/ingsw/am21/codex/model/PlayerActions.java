38d062e941b2f236abad8b0cea930ef8121b7f8a
hamza.haddou <hamza.haddou@mail.polimi.it>
2024-04-23 14:21:10 +0200

    feat: docs

---
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/PlayerActions.java
@@ -0,0 +1,18 @@
+package polimi.ingsw.am21.codex.model;
+
+public interface PlayerActions {
+    // will be implemented by the controller
+    // player init
+//    TokenColor chooseTokenColor(TokenColor availableColors, int choice);
+//    ObjectiveCard chooseObjectiveCard(ObjectiveCard[] cards, int choice);
+//
+//    // player plays a card
+//    SidedCard choosePlayingCard(int choice);
+//    CardSidesTypes choosePlayingCardSide(int choice);
+//    Position choosePlayingCardPositions(int x, int y);
+//
+//    //player draws a card
+//    DrawingSourceTypes chooseDrawingSource(int source);
+//    DrawingDeckTypes chooseDrawingDeck(int deck);
+
+}
