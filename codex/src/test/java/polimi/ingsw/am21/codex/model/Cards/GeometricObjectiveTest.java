362395cbae0c548f9c7dc0ea2797b09bcb30985a
frephs <francesco1.genovese@mail.polimi.it>
2024-04-30 00:02:36 +0200

    fix(): commit not running test for shared bugfix purposes

--- codex/src/test/java/polimi/ingsw/am21/codex/model/Cards/GeometricObjectiveTest.java
+++ codex/src/test/java/polimi/ingsw/am21/codex/model/Cards/GeometricObjectiveTest.java
@@ -2,32 +2,78 @@ package polimi.ingsw.am21.codex.model.Cards;
 
 import org.junit.jupiter.api.Test;
 import polimi.ingsw.am21.codex.model.Cards.Objectives.GeometricObjective;
+import polimi.ingsw.am21.codex.model.Cards.Objectives.Objective;
 import polimi.ingsw.am21.codex.model.Cards.Objectives.ObjectiveCard;
+import polimi.ingsw.am21.codex.model.Cards.Playable.CardSideType;
 import polimi.ingsw.am21.codex.model.Cards.Playable.PlayableCard;
 import polimi.ingsw.am21.codex.model.Player.PlayerBoard;
+import polimi.ingsw.am21.codex.model.PlayerBoardTest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static polimi.ingsw.am21.codex.model.Cards.CornerPosition.BOTTOM_LEFT;
+import static polimi.ingsw.am21.codex.model.Cards.CornerPosition.TOP_LEFT;
 
 class GeometricObjectiveTest {
 
   @Test
   void getEvaluator() {
     Map<AdjacentPosition, ResourceType> geometry = new HashMap<>();
-    geometry.put(BOTTOM_LEFT, ResourceType.FUNGI);
+
+    // test diagonal geometry
+    geometry.put(CornerPosition.TOP_LEFT, ResourceType.PLANT);
+    geometry.put(EdgePosition.CENTER, ResourceType.PLANT);
+    geometry.put(CornerPosition.BOTTOM_RIGHT, ResourceType.PLANT);
+
     GeometricObjective geometricObjective = new GeometricObjective(geometry);
 
+    PlayerBoardTest pbt = new PlayerBoardTest();
+    pbt.externalSetup();
+
     PlayerBoard pb = new PlayerBoard(
-      new ArrayList<>(),
-      new PlayableCard(123, null, null),
-      new ObjectiveCard(123, 123, null)
+      List.of(pbt.resourceCard,pbt.resourceCard, pbt.resourceCard),
+      pbt.resourceCard,
+      new ObjectiveCard(123, 3, new GeometricObjective(geometry))
     );
 
-    //evaluator should return 0
-    assertEquals(geometricObjective.getEvaluator().apply(pb, 123), 0);
+    assertEquals(pbt.resourceCard.getKingdom().orElseThrow(), ResourceType.PLANT);
+
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(-1,1));
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(1,-1));
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(2,-2));
+
+    // introducing an element of disturb
+    pb.getHand().add(pbt.resourceCard);
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(3,-1));
+
+    pb.getHand().add(pbt.resourceCard);
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(3,-3));
+
+    assertEquals(9, pb.getObjectiveCard().getEvaluator().apply(pb));
+
+    // testing "disconnected" cards geometry.
+    geometry = new HashMap<>();
+    geometry.put(TOP_LEFT, ResourceType.PLANT);
+    geometry.put(EdgePosition.CENTER, ResourceType.PLANT);
+    geometry.put(EdgePosition.BOTTOM, ResourceType.PLANT);
+
+    assertEquals(pbt.resourceCard.getKingdom().orElseThrow(), ResourceType.PLANT);
+
+    pb = new PlayerBoard(
+      List.of(pbt.resourceCard,pbt.resourceCard, pbt.resourceCard),
+      pbt.resourceCard,
+      new ObjectiveCard(123, 2, new GeometricObjective(geometry))
+    );
+
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(-1,1));
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(1,-1));
+    pb.placeCard(pbt.resourceCard,CardSideType.BACK, new Position(0,-2));
+
+    assertEquals(2, pb.getObjectiveCard().getEvaluator().apply(pb));
+
   }
 }
