8c8513a352138844c06c44e1ff1be1ad055c5ce0
frephs <francesco1.genovese@mail.polimi.it>
2024-05-14 14:50:33 +0200

    feat: client messages now extend its abstract message class

--- codex/src/main/java/polimi/ingsw/am21/codex/controller/messages/ActionMessage.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/controller/messages/ActionMessage.java
@@ -1,6 +1,6 @@
 package polimi.ingsw.am21.codex.controller.messages;
 
-public abstract class ActionMessage extends Message {
+public abstract class ActionMessage extends ClientMessage {
 
   public ActionMessage(MessageType type) {
     super(type);
--- codex/src/main/java/polimi/ingsw/am21/codex/controller/messages/RequestMessage.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/controller/messages/RequestMessage.java
@@ -1,6 +1,6 @@
 package polimi.ingsw.am21.codex.controller.messages;
 
-public abstract class RequestMessage extends Message {
+public abstract class RequestMessage extends ClientMessage {
 
   public RequestMessage(MessageType type) {
     super(type);
