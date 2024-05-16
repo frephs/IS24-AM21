7051398172bff646452569c880581ca7bd14b959
frephs <francesco1.genovese@mail.polimi.it>
2024-05-16 14:36:12 +0200

    view: make token color colorable

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Player/TokenColor.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Player/TokenColor.java
@@ -1,9 +1,23 @@
 package polimi.ingsw.am21.codex.model.Player;
 
-public enum TokenColor {
+import polimi.ingsw.am21.codex.view.TUI.utils.commons.Color;
+import polimi.ingsw.am21.codex.view.TUI.utils.commons.Colorable;
+
+public enum TokenColor implements Colorable {
   RED,
   BLUE,
   GREEN,
   YELLOW,
-  BLACK,
+  BLACK;
+
+  @Override
+  public Color getColor() {
+    return switch (this) {
+      case RED -> Color.RED;
+      case BLUE -> Color.BLUE;
+      case GREEN -> Color.GREEN;
+      case YELLOW -> Color.YELLOW;
+      case BLACK -> Color.BLACK;
+    };
+  }
 }
