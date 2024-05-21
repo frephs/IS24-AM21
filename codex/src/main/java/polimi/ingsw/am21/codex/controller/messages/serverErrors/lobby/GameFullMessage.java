16b8c56951e469eb9702705fcf5362fb37f2c0c7
frephs <francesco1.genovese@mail.polimi.it>
2024-05-21 12:54:26 +0200

    feat: add parameters to gameNotFound and gameFull messages

--- codex/src/main/java/polimi/ingsw/am21/codex/controller/messages/serverErrors/lobby/GameFullMessage.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/controller/messages/serverErrors/lobby/GameFullMessage.java
@@ -5,12 +5,19 @@ import polimi.ingsw.am21.codex.controller.messages.MessageType;
 
 public class GameFullMessage extends ErrorMessage {
 
-  public GameFullMessage() {
+  private final String gameId;
+
+  public GameFullMessage(String gameId) {
     super(MessageType.GAME_FULL);
+    this.gameId = gameId;
+  }
+
+  public String getGameId() {
+    return gameId;
   }
 
   @Override
   public String toString() {
-    return getType().toString();
+    return getType().toString() + "{ gameId=" + gameId + " }";
   }
 }
