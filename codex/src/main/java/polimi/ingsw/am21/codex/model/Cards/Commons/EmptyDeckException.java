1de5e45df609f9b23d2dda293bf4051dad35cd22
frephs <francesco1.genovese@mail.polimi.it>
2024-06-01 16:47:21 +0200

    chore: add message

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/Commons/EmptyDeckException.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/Commons/EmptyDeckException.java
@@ -3,6 +3,6 @@ package polimi.ingsw.am21.codex.model.Cards.Commons;
 public class EmptyDeckException extends Exception {
 
   public EmptyDeckException() {
-    super();
+    super("Deck is empty");
   }
 }
