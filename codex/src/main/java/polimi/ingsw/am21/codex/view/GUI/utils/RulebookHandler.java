68749305006b3fa1d3bbd8440514e6e4f98902f7
Federico Grandi <federico1.grandi@mail.polimi.it>
2024-06-06 22:07:42 +0200

    fix: fix compilation error in RulebookHandler

--- codex/src/main/java/polimi/ingsw/am21/codex/view/GUI/utils/RulebookHandler.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/view/GUI/utils/RulebookHandler.java
@@ -1,15 +1,16 @@
 package polimi.ingsw.am21.codex.view.GUI.utils;
 
+import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
-import java.util.List;
-
 public class RulebookHandler {
+
   @FXML
   private HBox pageContainer;
+
   @FXML
   private ImageView page;
 
@@ -18,7 +19,7 @@ public class RulebookHandler {
 
   public RulebookHandler() {
     //pages = List.of(new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-1-1.png"), new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-1-2.png"), new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-3-1.png"), new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-4-1.png"), new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-5-1.png"), new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-6-1.png"), new Image("codex/src/main/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-7-1.png"));
-      /*pages = List.of(
+    /*pages = List.of(
         new Image("/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-1-1.png"),
         new Image("/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-1-2.png"),
         new Image("/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-3-1.png"),
@@ -28,12 +29,13 @@ public class RulebookHandler {
         new Image("/resources/polimi/ingsw/am21/codex/view/GUI/pictures/rulebook/CODEX_Rulebook_EN-7-1.png")
       );
       page.setImage(pages.get(currentPage));*/
-    }
-
+    // TODO fix this
+    pages = List.of();
+  }
 
   @FXML
   private void handleNextPage() {
-    if(currentPage < pages.size() -1) {
+    if (currentPage < pages.size() - 1) {
       currentPage++;
       updatePageView();
     } else {
@@ -44,7 +46,7 @@ public class RulebookHandler {
 
   @FXML
   public void handlePreviousPage() {
-    if(currentPage > 0) {
+    if (currentPage > 0) {
       currentPage--;
       updatePageView();
     } else {
