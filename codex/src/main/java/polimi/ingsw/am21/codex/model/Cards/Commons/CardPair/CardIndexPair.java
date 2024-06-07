94c1607f2d151aba95bd2345aa2c09331dfcc6d9
hamza.haddou <hamza.haddou@mail.polimi.it>
2024-06-07 13:21:25 +0200

    feat: util method

--- codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/Commons/CardPair/CardIndexPair.java
+++ codex/src/main/java/polimi/ingsw/am21/codex/model/Cards/Commons/CardPair/CardIndexPair.java
@@ -1,8 +1,17 @@
 package polimi.ingsw.am21.codex.model.Cards.Commons.CardPair;
 
+import polimi.ingsw.am21.codex.model.Cards.Card;
+
 public class CardIndexPair extends CardPairBase<Integer> {
 
   public CardIndexPair(Integer firstCard, Integer secondCard) {
     super(firstCard, secondCard);
+  }
+
+  public static CardIndexPair fromCardPair(CardPair<Card> cardPair) {
+    return new CardIndexPair(
+      cardPair.getFirst().getId(),
+      cardPair.getSecond().getId()
+    );
   }
 }
